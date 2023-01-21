71590be6b86a257ef81a956afc6cd3e1206ef135
Abhijeet Kumar <akc972527@gmail.com>
2023-01-21 14:42:33 +0530

    implemented redux store

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 const Movies = () => {
-    console.log("Movies");
+  const { data } = useGetMoviesQuery();
+  console.log('data: ', data);
+  // console.log('Movies');
   return (
     <div>Movies</div>
-  )
-}
+  );
+};
 
 export default Movies;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+
+import store from './app/store';
 import App from './components/App';
 
 const theme = createTheme({});
@@ -9,11 +12,13 @@ const theme = createTheme({});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     /* eslint-disable */
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <React.StrictMode>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                {/* <React.StrictMode> */}
                 <App />
-            </React.StrictMode>
-        </BrowserRouter>
-    </ThemeProvider>,
+                {/* </React.StrictMode> */}
+            </BrowserRouter>
+        </ThemeProvider>,
+    </Provider>
 );
