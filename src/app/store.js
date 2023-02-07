6aa51e652477c57f76d3a13cc457b495239678a8
Abhijeet Kumar <akc972527@gmail.com>
2023-02-07 09:24:51 +0530

    implemented signup login functionality

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { tmdbApi } from '../services/TMDB';
 import genreOrCategoryReducer from '../features/currentGenreOrCategory';
+import useReducer from '../features/auth';
 
 export default configureStore({
     reducer: {
         currentGenreOrCategory: genreOrCategoryReducer,
+        user: useReducer,
         [tmdbApi.reducerPath]: tmdbApi.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tmdbApi.middleware),
